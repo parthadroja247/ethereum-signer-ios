db36e6ee52a9cb39de4100509aef2098afd16f80
Parth Adroja <parthadroja247@gmail.com>
2020-02-16 16:17:14 +0530

    Added UI for setup controller

---
+++ ethereum-signer/Sources/ViewControllers/SetupViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SetupViewController.swift
+//  ethereum-signer
+//
+//  Created by Parth Adroja on 16/02/20.
+//  Copyright © 2020 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController {
+    @IBOutlet var privateKeyTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+
+    private func configureView() {
+        configureNavigationBar()
+        configurePrivateKeyTextField()
+    }
+
+    private func configureNavigationBar() {
+        title = Constants.Title.setup
+        navigationItem.setHidesBackButton(true, animated: true)
+    }
+
+    private func configurePrivateKeyTextField() {
+        privateKeyTextField.placeholder = Constants.Placeholder.privateKey
+        privateKeyTextField.becomeFirstResponder()
+        privateKeyTextField.backgroundColor = .systemGray6
+        privateKeyTextField.delegate = self
+    }
+
+    private func privateKeyEntered(key: String) {
+        print("KEY == \(key)")
+    }
+}
+
+extension SetupViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = textField.text, !text.isEmpty else {
+            // No text entered
+            return false
+        }
+        privateKeyEntered(key: text)
+        return true
+    }
+}
