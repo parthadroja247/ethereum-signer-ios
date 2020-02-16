1e2e4aadd6e8df2c038c96d21639454f61f543c5
Parth Adroja <parthadroja247@gmail.com>
2020-02-16 15:24:48 +0530

    Added splash screen

---
+++ ethereum-signer/Sources/ViewControllers/StartViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StartViewController.swift
+//  ethereum-signer
+//
+//  Created by Parth Adroja on 15/02/20.
+//  Copyright © 2020 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigateToSetup()
+    }
+
+    func navigateToSetup() {
+        // Go to new screen
+    }
+}
